a9a4e4c4d950c2ec353b818d6b920546923c27a3
Nandy <nandhirajan.pandiaraj@byjus.com>
2021-05-01 09:48:53 +0530

    Adding and inactive button groups

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -1,60 +1,60 @@
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
 import Header from "./Header";
-import {Main} from "./Main";
+import { Main } from "./Main";
 import Sidebar from "./Sidebar";
 import Dashboard from '../pages/Dashboard';
 import Forms from "../pages/Forms";
-import Buttons from "../pages/Buttons";
+import Buttons from "../pages/buttons/Buttons";
 import Cards from "../pages/Cards";
 import Charts from "../pages/Charts";
 import Tables from "../pages/Tables";
 import Modals from "../pages/Modals";
 
-function Layout(){
+function Layout() {
     return (
-       <div className="flex flex-row h-screen">
-        <Sidebar/>
-        <main className="flex flex-col w-full">
-            <Header/>
-            <Main>
-                <Route
-                    exact={true}
-                    path={`/app/dashboard`}
-                    component={Dashboard}
-                />
-                <Route
-                    exact={true}
-                    path={`/app/forms`}
-                    component={Forms}
-                />
-                <Route
-                    exact={true}
-                    path={`/app/buttons`}
-                    component={Buttons}
-                />                
-                <Route
-                    exact={true}
-                    path={`/app/cards`}
-                    component={Cards}
-                />
-                <Route
-                    exact={true}
-                    path={`/app/charts`}
-                    component={Charts}
-                />   
-                <Route
-                    exact={true}
-                    path={`/app/modals`}
-                    component={Modals}
-                />                          
-                <Route
-                    exact={true}
-                    path={`/app/tables`}
-                    component={Tables}
-                />                               
-            </Main>
-        </main>
-       </div>
+        <div className="flex flex-row h-screen">
+            <Sidebar />
+            <main className="flex flex-col w-full">
+                <Header />
+                <Main>
+                    <Route
+                        exact={true}
+                        path={`/app/dashboard`}
+                        component={Dashboard}
+                    />
+                    <Route
+                        exact={true}
+                        path={`/app/forms`}
+                        component={Forms}
+                    />
+                    <Route
+                        exact={true}
+                        path={`/app/buttons`}
+                        component={Buttons}
+                    />
+                    <Route
+                        exact={true}
+                        path={`/app/cards`}
+                        component={Cards}
+                    />
+                    <Route
+                        exact={true}
+                        path={`/app/charts`}
+                        component={Charts}
+                    />
+                    <Route
+                        exact={true}
+                        path={`/app/modals`}
+                        component={Modals}
+                    />
+                    <Route
+                        exact={true}
+                        path={`/app/tables`}
+                        component={Tables}
+                    />
+                </Main>
+            </main>
+        </div>
     )
 }
 
